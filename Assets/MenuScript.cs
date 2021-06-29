4917beda9113f8ef785bfd03ef75617b6cac5573
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2021-06-29 19:13:48 +0300

    Added basic connection approval.

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 using MLAPI;
 using UnityEngine.UI;
 using MLAPI.Transports.UNET;
+using System;
 
 public class MenuScript : MonoBehaviour
 {
     public GameObject MenuPanel;
     public InputField InputField;
+
+    public void Start()
+    {
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+    }
+
+    private void ApprovalCheck(byte[] connectionData, ulong clientID, MLAPI.NetworkManager.ConnectionApprovedDelegate callback)
+    {
+        bool approved = false;
+
+        // connection data is approved we join
+        string password = System.Text.Encoding.ASCII.GetString(connectionData);
+
+        if (password == "mygame")
+        {
+            approved = true;
+        }
+
+        callback(true, null, approved,new Vector3(0,4,0), Quaternion.identity);
+    }
 
     public void HostButton()
     {
@@ -27,7 +48,11 @@ public class MenuScript : MonoBehaviour
             // clicked join
             NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = InputField.text;
         }
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes("mygame");
+
         NetworkManager.Singleton.StartClient();
         MenuPanel.SetActive(false);
     }
+
+
 }
