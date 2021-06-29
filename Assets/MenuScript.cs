2c0411c1720b2f169119296ba0906af98365223d
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2021-06-29 18:40:51 +0300

    Made so the person can join using IP. By setting the IP adress of the host. Joins local host if the input field is left blank.

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using UnityEngine.UI;
+using MLAPI.Transports.UNET;
 
 public class MenuScript : MonoBehaviour
 {
     public GameObject MenuPanel;
+    public InputField InputField;
 
     public void HostButton()
     {
@@ -15,6 +18,15 @@ public class MenuScript : MonoBehaviour
 
     public void JoinButton()
     {
+        if (InputField.text.Length <= 0)
+        {
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "127.0.0.1";
+        }
+        else
+        {
+            // clicked join
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = InputField.text;
+        }
         NetworkManager.Singleton.StartClient();
         MenuPanel.SetActive(false);
     }
